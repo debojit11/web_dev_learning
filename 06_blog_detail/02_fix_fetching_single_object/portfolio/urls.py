26be652dd9ad2a88202c7f72e92943cb6448c604
Debojit <debojitchoudhury117@gmail.com>
2024-10-22 16:07:20 +0000

    assigment done and added gitignore

--- 06_blog_detail/02_fix_fetching_single_object/portfolio/urls.py
+++ 06_blog_detail/02_fix_fetching_single_object/portfolio/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', include('projects.urls')),
+    path('', lambda request: redirect('projects:projects')),
 ]
+
--- 06_blog_detail/02_fix_fetching_single_object/projects/views.py
+++ 06_blog_detail/02_fix_fetching_single_object/projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Project
 
 
@@ -7,6 +7,7 @@ def index(request):
     return render(request, 'projects/index.html', {'projects': projects})
 
 
-def detail(request):
+def detail(request, pk):
     # TODO: how can I get a single object from the database again?
+    project = get_object_or_404(Project, pk=pk)
     return render(request, 'projects/detail.html', {'project': project})
