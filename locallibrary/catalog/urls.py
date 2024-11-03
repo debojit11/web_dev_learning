cea23f67a2a4561357affdb1e4c5d02c5b540132
Debojit <debojitchoudhury117@gmail.com>
2024-11-03 12:30:29 +0000

    authentication done

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),  # Author list view
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),  # Author detail view
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 ]
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 def index(request):
@@ -25,6 +26,11 @@ def index(request):
     # Count of genres containing the search word (case-insensitive)
     num_genres_with_word = Genre.objects.filter(name__icontains=search_word).count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
@@ -33,6 +39,7 @@ def index(request):
         'num_books_with_word': num_books_with_word,
         'num_genres_with_word': num_genres_with_word,
         'search_word': search_word,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
@@ -58,3 +65,17 @@ class AuthorDetailView(generic.DetailView):
         context = super().get_context_data(**kwargs)
         context['books'] = Book.objects.filter(author=self.object)  # Retrieve books by the author
         return context
+    
+
+class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return (
+            BookInstance.objects.filter(borrower=self.request.user)
+            .filter(status__exact='o')
+            .order_by('due_back')
+        )
